706a6b4bbf460caf64d8fed5cff60757bf77bec9
nyashachimutapira <chimutapiran@gmail.com>
2024-09-22 03:33:37 +0200

    add changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+        Console.WriteLine(first);
     }
 }
