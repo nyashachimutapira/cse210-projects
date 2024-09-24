88df52fba4ce7ccbf01706c6e6d8c7f708122ced
nyashachimutapira <chimutapiran@gmail.com>
2024-09-24 10:11:24 +0200

    push

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create job instances
+        Job job1 = new Job("Software Engineer", "Microsoft", 2017, 2020);
+        Job job2 = new Job("Manager", "Sumsung", 2020, 2023);
+
+        // Create a resume instance
+        Resume myResume = new Resume("Nyasha Chimutapira");
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
